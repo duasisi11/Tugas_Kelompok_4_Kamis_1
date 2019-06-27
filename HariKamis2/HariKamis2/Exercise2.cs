21405196e39dacc9485b80003b6820bc9434b59c
Dua Sisi <sisi.dua11@gmail.com>
2019-06-27 15:20:53 +0700

    edit nama file

--- HariKamis2/HariKamis2/Exercise2.cs
+++ HariKamis2/HariKamis2/Exercise2.cs
@@ -10,47 +10,42 @@ namespace HariKamis2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-= Nama Hari =- ");
-            Console.WriteLine("1.Senin");
-            Console.WriteLine("2.Selasa");
-            Console.WriteLine("3.Rabu");
-            Console.WriteLine("4.Kamis");
-            Console.WriteLine("5.Jum'at");
-            Console.WriteLine("6.Sabtu");
-
-            Console.WriteLine("7.Minggu");
-            Console.Write("Input angka sesuai nama hari : ");
-            int pilih = int.Parse(Console.ReadLine());
-
-            switch (pilih)
-            {
-                case 1 : 
-                    Console.WriteLine("Hari Senin");
+           
+            //int counter = 0;
+            bool flag = true;
+            while(flag == true){
+                Console.WriteLine("----------=GAME=---------- ");
+                Console.WriteLine("1. Play Game ");
+                Console.WriteLine("2. Intruction ");
+                Console.WriteLine("3. Exit ");
+                Console.Write("Input angka : ");
+                int a = int.Parse(Console.ReadLine());
+                
+                if (a == 1)
+                {
+                    Console.WriteLine("Mulai Permainan");
+                    Console.ReadKey();
+                }
+                else if (a == 2)
+                {
+                    Console.WriteLine("Instruksi Permainan");                 
+                    Console.ReadKey();
+                }
+                else if (a == 3)
+                {
+                    flag = false;
                     break;
-                case 2 :
-                    Console.WriteLine("Hari Selasa");
-                    break;
-                case 3 :
-                    Console.WriteLine("Hari Rabu");
-                    break;
-                case 4 :
-                    Console.WriteLine("Hari Kamis");
-                    break;
-                case 5 :
-                    Console.WriteLine("Hari Jum'at");
-                    break;
-                case 6 :
-                    Console.WriteLine("Hari Sabtu");
-                    break;
-                case 7 :
-                    Console.WriteLine("Hari Minggu");
-                    break;
-                default :
-                    Console.WriteLine("Anda tidak memilih hari");
-                    break;
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Selain angka 1,2, dan 3");
+                    Console.ReadKey();
+                }
+                //counter++;
             }
-            Console.ReadKey();
-
-        }         
+            
+        }
+                
     }
 }
---
+++ HariKamis2/HariKamis2/SwitchCase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HariKamis2
+{
+    class SwitchCase
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-= Nama Hari =- ");
+            Console.WriteLine("1.Senin");
+            Console.WriteLine("2.Selasa");
+            Console.WriteLine("3.Rabu");
+            Console.WriteLine("4.Kamis");
+            Console.WriteLine("5.Jum'at");
+            Console.WriteLine("6.Sabtu");
+
+            Console.WriteLine("7.Minggu");
+            Console.Write("Input angka sesuai nama hari : ");
+            int pilih = int.Parse(Console.ReadLine());
+
+            switch (pilih)
+            {
+                case 1 : 
+                    Console.WriteLine("Hari Senin");
+                    break;
+                case 2 :
+                    Console.WriteLine("Hari Selasa");
+                    break;
+                case 3 :
+                    Console.WriteLine("Hari Rabu");
+                    break;
+                case 4 :
+                    Console.WriteLine("Hari Kamis");
+                    break;
+                case 5 :
+                    Console.WriteLine("Hari Jum'at");
+                    break;
+                case 6 :
+                    Console.WriteLine("Hari Sabtu");
+                    break;
+                case 7 :
+                    Console.WriteLine("Hari Minggu");
+                    break;
+                default :
+                    Console.WriteLine("Anda tidak memilih hari");
+                    break;
+            }
+            Console.ReadKey();
+
+        }         
+    }
+}
