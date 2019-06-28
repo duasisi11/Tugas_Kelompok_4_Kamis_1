d444b018c2fec0f4dedbeda6eb50d2b134b3a8a1
Dua Sisi <sisi.dua11@gmail.com>
2019-06-28 11:40:25 +0700

    Menambah project HariJumat2

---
+++ HariJumat2/HariJumat2/ArrayCode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HariJumat2
+{
+    class ArrayCode
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi array int.
+            int[] intArray;
+            // mengalokasikan memory untuk 5 nilai.
+            intArray = new int[5];
+            // inisialisasi array
+            intArray[0] = 10;
+            intArray[1] = 20;
+            intArray[2] = 30;
+            intArray[3] = 40;
+            intArray[4] = 50;
+
+            // mengakses elemen array dengan For
+            Console.Write("For loop :");
+            for (int i = intArray.Length-1; i >= 0; i--)
+            {
+
+                Console.Write(" " + intArray[i]);
+            }
+
+            Console.WriteLine(" ");
+
+            int j = intArray.Length-1;
+            Console.Write("For while :");
+            while (j >= 0)
+            {
+                Console.Write(" " + intArray[j]);
+                j--;
+            } 
+
+
+            /* Console.WriteLine("");
+             Console.Write("For-each loop :");
+             // menggunakan foreach
+             foreach (int i in intArray)
+             {
+
+                 Console.Write(" " + i);
+             } */
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ HariJumat2/HariJumat2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HariJumat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi array int.
+            int[] intArray;
+            // mengalokasikan memory untuk 5 nilai.
+            intArray = new int[5];
+            // inisialisasi array
+            intArray[0] = 10;
+            intArray[1] = 20;
+            intArray[2] = 30;
+            intArray[3] = 40;
+            intArray[4] = 50;
+
+            // mengakses elemen array dengan For
+            Console.Write("For loop :");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+
+                Console.Write(" " + intArray[i]);
+            }
+
+            Console.WriteLine("");
+            Console.Write("For-each loop :");
+            // menggunakan foreach
+            foreach (int i in intArray)
+            {
+
+                Console.Write(" " + i);
+            }
+
+            Console.WriteLine("");
+            Console.Write("while loop :");
+            // menggunakan while
+            int j = 0;
+            while (j < intArray.Length)
+            {
+
+                Console.Write(" " + intArray[j]);
+
+                j++;
+
+            }
+
+            Console.WriteLine("");
+            Console.Write("Do-while loop :");
+            // menggunakan do-while
+            int k = 0;
+            do
+            {
+                Console.Write(" " + intArray[k]);
+                k++;
+            } while (k < intArray.Length);
+
+            Console.ReadKey();
+
+        }
+    }
+}
