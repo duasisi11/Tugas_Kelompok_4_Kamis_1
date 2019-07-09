755148b70f8cd194e6dbafee9a1b58cefc7c0a68
Dua Sisi <sisi.dua11@gmail.com>
2019-07-09 15:05:17 +0700

    tambah materi 09/07/2019

---
+++ HariSelasa3/HariSelasa3/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace HariSelasa3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBackground = new System.Windows.Forms.Button();
+            this.btnTeks = new System.Windows.Forms.Button();
+            this.btnForm = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnBackground
+            // 
+            this.btnBackground.Location = new System.Drawing.Point(54, 103);
+            this.btnBackground.Name = "btnBackground";
+            this.btnBackground.Size = new System.Drawing.Size(75, 23);
+            this.btnBackground.TabIndex = 1;
+            this.btnBackground.Text = "Background";
+            this.btnBackground.UseVisualStyleBackColor = true;
+            this.btnBackground.Click += new System.EventHandler(this.btnBackground_Click);
+            // 
+            // btnTeks
+            // 
+            this.btnTeks.Location = new System.Drawing.Point(146, 103);
+            this.btnTeks.Name = "btnTeks";
+            this.btnTeks.Size = new System.Drawing.Size(75, 23);
+            this.btnTeks.TabIndex = 2;
+            this.btnTeks.Text = "Teks";
+            this.btnTeks.UseVisualStyleBackColor = true;
+            this.btnTeks.Click += new System.EventHandler(this.btnTeks_Click);
+            // 
+            // btnForm
+            // 
+            this.btnForm.Location = new System.Drawing.Point(12, 12);
+            this.btnForm.Name = "btnForm";
+            this.btnForm.Size = new System.Drawing.Size(75, 23);
+            this.btnForm.TabIndex = 3;
+            this.btnForm.Text = "Form";
+            this.btnForm.UseVisualStyleBackColor = true;
+            this.btnForm.Click += new System.EventHandler(this.btnForm_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(241, 103);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(75, 23);
+            this.btnFont.TabIndex = 4;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.LightGray;
+            this.btnOpen.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnOpen.Location = new System.Drawing.Point(54, 146);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 25);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(146, 148);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(241, 148);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "Browse File";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(432, 94);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(213, 32);
+            this.btnPrint.TabIndex = 8;
+            this.btnPrint.Text = "Print File";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(493, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(429, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Isikan Teks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Username";
+            // 
+            // textUsername
+            // 
+            this.textUsername.Location = new System.Drawing.Point(137, 23);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.Size = new System.Drawing.Size(168, 20);
+            this.textUsername.TabIndex = 12;
+            this.textUsername.Validating += new System.ComponentModel.CancelEventHandler(this.textUsername_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Password";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(137, 59);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(168, 20);
+            this.textPassword.TabIndex = 14;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(230, 99);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 15;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textUsername);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textPassword);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(137, 274);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(337, 166);
+            this.panel1.TabIndex = 16;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 513);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnForm);
+            this.Controls.Add(this.btnTeks);
+            this.Controls.Add(this.btnBackground);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBackground;
+        private System.Windows.Forms.Button btnTeks;
+        private System.Windows.Forms.Button btnForm;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ HariSelasa3/HariSelasa3/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//tambahin
+using System.Drawing.Printing;
+
+namespace HariSelasa3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ColorDialog cd = new ColorDialog();
+
+        private void btnBackground_Click(object sender, EventArgs e)
+        {
+            cd.ShowDialog();
+            textBox1.BackColor = cd.Color;
+        }
+
+        private void btnTeks_Click(object sender, EventArgs e)
+        {
+            cd.ShowDialog();
+            textBox1.ForeColor = cd.Color;
+        }
+
+        private void btnForm_Click(object sender, EventArgs e)
+        {
+            cd.ShowDialog();
+            this.BackColor = cd.Color;
+        }
+
+        FontDialog fd = new FontDialog();
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            fd.ShowDialog();
+            textBox1.Font = fd.Font;
+
+            /*fd.ShowDialog();
+            btnOpen.Font = fd.Font;*/
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.ShowDialog();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fdb = new FolderBrowserDialog();
+            fdb.ShowDialog();
+        }
+
+        PrintDialog pd = new PrintDialog();
+        internal PrintDocument pdoc = new PrintDocument();
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            pd.Document = pdoc;
+            DialogResult result = pd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                pdoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(pdoc_PrintPage);
+               
+                pdoc.Print();
+            }
+        }
+
+        private void pdoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(textBox2.Text, new Font("Arial", 40, FontStyle.Bold), Brushes.Black, 150, 125);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                MessageBox.Show(textUsername.Text, "Message (Pesan)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void textUsername_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textUsername.Text))
+            {
+                e.Cancel = true;
+                textUsername.Focus();
+                errorProvider1.SetError(textUsername, "Please enter your Username !");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(textUsername, null);
+            }
+        }
+
+
+    }
+}
